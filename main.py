5eb8c3486c68b99eb77106dee716fbeecc67fd8f
Ikwuka Okoye <ikwukao@proton.me>
2026-05-14 12:46:09 -0400

    Encapsulate function body with for loop and change its parameter

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-def pin_extractor(poem):
-    secret_code = ""
-    lines = poem.split('\n')
-
-    for line_index, line in enumerate(lines):
-        words = line.split()
-        
-        if len(words) > line_index:
-            secret_code += str(len(words[line_index]))
-        else:
-            secret_code += "0"
+def pin_extractor(poems):
     
-    return secret_code
+    for poem in poems:
+        secret_code = ""
+        lines = poem.split('\n')
+
+        for line_index, line in enumerate(lines):
+            words = line.split()
+            
+            if len(words) > line_index:
+                secret_code += str(len(words[line_index]))
+            else:
+                secret_code += "0"
+        
+        return secret_code
 
 poem = "Stars and the moon\nshine in the sky\nwhite and bright\nuntil the end of the night"
 poem2 = "The grass is green\nhere and there\nhoping for rain\nbefore it turns yellow"

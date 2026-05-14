97f703225a108cbab56466c7a76491198058cbda
Ikwuka Okoye <ikwukao@proton.me>
2026-05-14 11:56:57 -0400

    Test pin_extractor function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 def pin_extractor(poem):
-    pass
+    secret_code = ""
+
+
+poem = """ 
+        Stars and the moon\n
+        shine in the sky\n
+        white and bright\n
+        until the end of the night 
+       """
+
+pin_extractor(poem)

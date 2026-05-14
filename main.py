9dfb18a695c7f9ba32bfc0a1f2332b24c28bb751
Ikwuka Okoye <ikwukao@proton.me>
2026-05-14 12:27:53 -0400

    Return secret_code and print to the stdout

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ def pin_extractor(poem):
     lines = poem.split('\n')
 
     for line_index, line in enumerate(lines):
-        print(line_index, line)
         words = line.split()
         secret_code += str(len(words[line_index]))
-
+    return secret_code
 
 poem = "Stars and the moon\nshine in the sky\nwhite and bright\nuntil the end of the night"
 
-pin_extractor(poem)
+print(pin_extractor(poem))

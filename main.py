3e93eac05a1026892e394c51c13329556f7a40ca
Ikwuka Okoye <ikwukao@proton.me>
2026-05-14 12:12:06 -0400

    Use the enumerate method to iterate through the loop

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ def pin_extractor(poem):
     secret_code = ""
     lines = poem.split('\n')
 
-    for line in lines:
-        print(line) 
+    for line_index, line in enumerate(lines):
+        print(line_index, line)
 
 
-poem = """ 
-        Stars and the moon\n
-        shine in the sky\n
-        white and bright\n
-        until the end of the night 
-       """
+poem = "Stars and the moon\nshine in the sky\nwhite and bright\nuntil the end of the night"
 
 pin_extractor(poem)

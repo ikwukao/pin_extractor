9f96f390255a63558fe2f3b7aa38366de101ac4b
Ikwuka Okoye <ikwukao@proton.me>
2026-05-14 12:22:11 -0400

    Update 'secret_code' with augmented assignment

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def pin_extractor(poem):
 
     for line_index, line in enumerate(lines):
         print(line_index, line)
+        words = line.split()
+        secret_code += str(len(words[line_index]))
 
 
 poem = "Stars and the moon\nshine in the sky\nwhite and bright\nuntil the end of the night"
